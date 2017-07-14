63fb76010dda3c2e02dfd181348c13a5f6209767
Francisco José Gómez Arnal <fjgomez@florida-uni.es>
2017-07-14 12:12:17 +0200

    Primer  proyecto

---
+++ PruebaFor/src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<10;i++) {
+			System.out.println("Paco es "+i+" veces tonto");
+		}
+	}
+
+}
